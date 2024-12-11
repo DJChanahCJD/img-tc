5b4bd754d606ae9a25d6bca2369ce6684c2b141f
浪小矢 <djchan411130@gmail.com>
2024-12-11 10:57:50 +0800

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -14,8 +14,8 @@ export async function onRequestPost(context) {
             throw new Error('No file uploaded');
         }
 
-        const settings = await fetch('./api/settings');
         const url = new URL(request.url);
+        const settings = await fetch(`${url.origin}/api/settings`);
         const isAdmin = request.headers.get('Referer')?.includes(`${url.origin}/admin`);
 
         if (!settings.uploadPublic && !isAdmin) {
